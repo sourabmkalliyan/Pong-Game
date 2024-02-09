2f905e4ee5f92c814579c18f2100bdc9249a47b7
sourabmkalliyan <sourabmkalliyanofficial@gmail.com>
2024-02-10 03:48:41 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+screen.listen()
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+screen.exitonclick()
